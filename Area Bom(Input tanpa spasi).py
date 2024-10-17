f200f5b382c7ae9346616e8450217ac0260d463f
Wayan Raditya Putra <radityaputraa1122@gmail.com>
2024-10-17 18:43:25 +0700

    Add files via upload

---
+++ Area Bom(Input tanpa spasi).py
@@ -0,0 +1,42 @@
+#Input nilai 
+N, M = map(int, input().split())
+
+#Input array 2d bernama lokasi
+lokasi = [list(input()) for _ in range(N)]
+
+#Input array S untuk menyimpan semua nilai di sekitar lokasi bom
+S = []
+
+#Melakukan for loop untuk pengecekan :
+for i in range(N):
+    for j in range(M):
+        if lokasi[i][j] == '1':
+            if i - 1 >= 0:
+                if lokasi[i-1][j] == '0':
+                    S.append(lokasi[i-1][j])
+            if j - 1 >= 0:
+                if lokasi[i][j-1] == '0':
+                    S.append(lokasi[i][j-1])
+            if i + 1 <= N - 1:
+                if lokasi[i+1][j] == '0':
+                    S.append(lokasi[i+1][j])
+            if j + 1 <= M - 1:
+                if lokasi[i][j+1] == '0':
+                    S.append(lokasi[i][j+1])
+            if i - 1 >= 0 and j - 1 >= 0:
+                if lokasi[i-1][j-1] == '0':
+                    S.append(lokasi[i-1][j-1])
+            if i - 1 >= 0 and j + 1 <= M - 1:
+                if lokasi[i-1][j+1] == '0':
+                    S.append(lokasi[i-1][j+1])
+            if i + 1 <= N - 1  and j - 1 >= 0:
+                if lokasi[i+1][j-1] == '0':
+                    S.append(lokasi[i+1][j-1])
+            if i + 1 <= N - 1 and j + 1 <= M - 1:
+                if lokasi[i+1][j+1] == '0':
+                    S.append(lokasi[i+1][j+1])
+            
+
+#Print output
+print(len(S))
+print(lokasi)
